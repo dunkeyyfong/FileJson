4bcd7ab2934bb79fbd465c00c106405e21edca22
dunkeyyfong <phongtrong865@gmail.com>
2023-09-01 12:01:49 +0700

    Initial Commit

---
+++ FILEJSOn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FILEJSOn
+//
+//  Created by DunkeyyFong on 01/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FILEJSOn/FILEJSOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  FILEJSOnApp.swift
+//  FILEJSOn
+//
+//  Created by DunkeyyFong on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FILEJSOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
